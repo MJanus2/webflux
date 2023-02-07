d8206c7e2140c537d4fbd232c836b74cf9b5f2a1
Mateusz <mateusz.a.janus@outlook.com>
2023-02-07 11:03:44 +0100

    deleted unused import

--- src/main/java/com/mjanus/webflux/controller/BookController.java
+++ src/main/java/com/mjanus/webflux/controller/BookController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
 import java.util.List;
 
 @RestController
